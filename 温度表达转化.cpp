c2622761359a6041debfd0852c3cf7ba7828b80f
quqi <2484667023@qq.com>
2019-01-29 08:31:46 +0800

    add some code

--- 温度表达转化.cpp
+++ 温度表达转化.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    long long int f=0;
-    double c=0;
-    cin>>f;
-    c=5*(f-32)/9;
-    printf("%.5f\n",c);
-    return 0;
+int main()
+{
+double f,c;
+cin>>f;
+c=5*(f-32)/9;
+printf("%.5f",c);
+return 0;
 }
