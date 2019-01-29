2364ee905a592c57473a3fd84757434f49097359
quqi <2484667023@qq.com>
2019-01-29 11:19:30 +0800

    bubble sort

---
+++ bubble sort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,j,t,a[1005];
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    for(i=1;i<n;i++)
+		for(j=n;j>i;j--)
+			if(a[j]<a[j-1])
+			{
+				t=a[j];
+				a[j]=a[j-1];
+				a[j-1]=t;
+			}
+        }
+    }
+    for(i=1;i<=n;i++){
+		cout<<a[i]<<" ";
+    }
+    return 0;
+}
