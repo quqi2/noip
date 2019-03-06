83b07bd1ffc95bb9c588dac6de486e4bfb348f3c
quqi <2484667023@qq.com>
2019-03-06 21:27:30 +0800

    add 点和正方形的关系

---
+++ 点和正方形的关系.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){ 
+    long long x,y;
+    cin>>x>>y;
+    if(x<=1&&x>=-1&&y<=1&&y>=-1){
+        printf("yes");
+    }
+    else{
+        printf("no"); 
+    }
+    return 0;
+}
