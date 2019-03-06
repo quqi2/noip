e0f91700513e0d84a6f38fdeacf547eb6e943458
quqi <2484667023@qq.com>
2019-03-06 21:06:58 +0800

    add some code

---
+++ 输入半径计算球的体积.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double r=0.0,ans=0.0;
+    const double pie=3.14;
+    cin>>r;
+    ans=pie*r*r*r*4.0/3.0;
+    printf("%.2lf",ans);
+    return 0;
+}
