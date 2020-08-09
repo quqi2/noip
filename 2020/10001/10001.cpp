216896f07e47ed531860e554b402a942eb0ea96d
“quqi2” <2484667023@qq.com>
2020-08-09 16:44:26 +0800

    add some code

---
+++ 2020/10001/10001.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("10001.in","r",stdin);
+    freopen("10001.out","w",stdout);
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+
+} 
