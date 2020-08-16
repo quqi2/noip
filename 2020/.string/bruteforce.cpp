7fefb35b807ad2a53d0780500161ac7f97af0912
quqi2 <2484667023@qq.com>
2020-08-16 11:35:22 +0800

    add code

--- 2020/.string/bruteforce.cpp
+++ 2020/.string/bruteforce.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 int main(){
     string s="BBC ABCDAB ABCDABCDABDE",p="ABCDABD";
+    int index=0;
     for(int i=0;i<s.length();i++){
         for(int j=0;j<p.length();j++){
+           if(s[index]==p[1]){
+               /*继续匹配*/
+            } 
+            else
+            {
+                break;
+            }
             
         }
+        index++;
     }
 }
