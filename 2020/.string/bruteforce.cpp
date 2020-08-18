23fbd5f354504dcd986cf5d457c47896cb597dfb
quqi2 <2484667023@qq.com>
2020-08-18 20:56:57 +0800

    message

--- 2020/.string/bruteforce.cpp
+++ 2020/.string/bruteforce.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-int bf(string inputtext,string inputkeyword){
-    int sizet=inputtext.size(),sizek=inputkeyword.size();
+int bf(string text,string  keyword){
+    int sizet=text.size(),sizek= keyword.size();
     
     for(int i=0;i<sizet;i++){
         int j;
         for(j=0;j<sizek;j++){
-            if(inputtext[i+j]!=inputkeyword[j]) break;
+            if(text[i+j]!= keyword[j]) break;
         }
         if(j==sizek){
             return i;
